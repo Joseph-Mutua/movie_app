fc31132822445c52f75aac428e196fb4acf25423
Joseph Mutua <mutuaj793@gmail.com>
2021-06-20 12:46:28 +0300

    Fnish project setup

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const helmet = require("helmet");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(helemt());
+app.use(helmet());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
