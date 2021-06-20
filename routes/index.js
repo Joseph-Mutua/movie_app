0a820d9f8a52d78c9642496f3a23a4fbb65986e2
Joseph Mutua <mutuaj793@gmail.com>
2021-06-20 13:08:51 +0300

    Add the axios module

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const axios = require("axios");
+
+const apiKey = "3b590e7bad40ab5efefeac4771f25bb0";
+const apiBaseUrl = "https://api.themoviedb.org/3";
+const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
+const imageBaseUrl = "https://image.tmdb.org/t/p/w300";
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { });
+router.get("/",  (req, res, next) => {
+  axios
+    .get(nowPlayingUrl)
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  res.render("index", {});
 });
 
 module.exports = router;
