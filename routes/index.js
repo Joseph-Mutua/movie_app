4375cf413c5c14c3bd5b2ed34bccf101989210bc
Joseph Mutua <mutuaj793@gmail.com>
2021-06-20 14:14:20 +0300

    putting the movie data in a template

--- routes/index.js
+++ routes/index.js
@@ -5,19 +5,22 @@ const axios = require("axios");
 const apiKey = "3b590e7bad40ab5efefeac4771f25bb0";
 const apiBaseUrl = "https://api.themoviedb.org/3";
 const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
-const imageBaseUrl = "https://image.tmdb.org/t/p/w300";
+const imageBaseUrl = "https://image.tmdb.org/t/p/w500";
+
+router.use((req, res, next) => {
+  res.locals.imageBaseUrl = imageBaseUrl;
+  next();
+});
 
 /* GET home page. */
-router.get("/",  (req, res, next) => {
-  axios
-    .get(nowPlayingUrl)
-    .then((res) => {
-      console.log(res.data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  res.render("index", {});
+router.get("/", async (req, res, next) => {
+  try {
+    const response = await axios.get(nowPlayingUrl);
+    const movieData = response.data.results;
+    res.render("index", { movieData });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
